6b781042eeb260206c2e08ba8da3243c103a3cae
Ramesh <Ramesh@RAMESH>
2021-06-29 16:39:28 +0530

    getters and setters for instance variables

---
+++ GetterSetterDemo/src/GetSetDemo.java
@@ -0,0 +1,40 @@
+
+public class GetSetDemo {
+	String CustName;
+    int CustId;
+    int CustTransaactionDetails;
+    String CustAddr;
+    int MobileNumber;
+	public String getCustName() {
+		return CustName;
+	}
+	public void setCustName(String custName) {
+		CustName = custName;
+	}
+	public int getCustId() {
+		return CustId;
+	}
+	public void setCustId(int custId) {
+		CustId = custId;
+	}
+	public int getCustTransaactionDetails() {
+		return CustTransaactionDetails;
+	}
+	public void setCustTransaactionDetails(int custTransaactionDetails) {
+		CustTransaactionDetails = custTransaactionDetails;
+	}
+	public String getCustAddr() {
+		return CustAddr;
+	}
+	public void setCustAddr(String custAddr) {
+		CustAddr = custAddr;
+	}
+	public int getMobileNumber() {
+		return MobileNumber;
+	}
+	public void setMobileNumber(int mobileNumber) {
+		MobileNumber = mobileNumber;
+	}
+    
+    
+}
